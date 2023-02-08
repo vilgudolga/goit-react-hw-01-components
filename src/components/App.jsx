f0f4bb01338bc4d83076923fded71d7466492b2d
Olga <vilgudolga@gmail.com>
2023-02-08 20:33:14 +0100

    Up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import Profile from "./Profile/profile";
-import user from "./Profile/user.json";
+import Profile from './Profile/profile';
+import user from './Profile/user.json';
 
-import Statistics from "./Statistics/Statistics";
-import data from "./Statistics/data.json";
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
-import FriendList from "./FriendList/FriendList";
-import friends from "./FriendList/friends.json";
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
 
-import TransactionHistory from "./Transactions/TransactionHistory.jsx";
-import transactions from "./Transactions/transactions.json";
+import TransactionHistory from './Transactions/TransactionHistory.jsx';
+import transactions from './Transactions/transactions.json';
 
 export const App = () => {
   return (
@@ -23,6 +23,6 @@ export const App = () => {
       <Statistics title="upload status" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
-      </div>
+    </div>
   );
 };
