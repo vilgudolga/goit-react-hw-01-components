6f19d33335866fd7c1b87d42d99b1daa11b91625
Olga <vilgudolga@gmail.com>
2023-02-08 20:45:08 +0100

    UP

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import styles from "./statistics.module.css";
+import PropTypes from 'prop-types';
+import styles from './statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
-    return (
-        <div className={styles.statistics}>
-            <h2 className={styles.title}>{title}</h2>
-            <ul className={styles.statList}>
-                {stats.map(({ id, label, percentage }) => (
-                    <li className={styles.item} key={id}>
-                        <span className={styles.label}>{label}</span>
-                        <span className={styles.percentage}>{percentage}%</span>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-
+  return (
+    <div className={styles.statistics}>
+      <h2 className={styles.title}>{title}</h2>
+      <ul className={styles.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={styles.item} key={id}>
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            label: PropTypes.string.isRequired,
-            percentage: PropTypes.number.isRequired,
-        })
-    ),
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default Statistics;
